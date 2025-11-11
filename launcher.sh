b22a87a9d7905371896e53b0593bb9db533194b2
Pierre Parent <pierre.parent@pparent.fr>
2025-11-11 08:34:14 +0100

    Re-enable GPU to some extend for confined app

--- launcher.sh
+++ launcher.sh
@@ -6,7 +6,6 @@ export GTK_IM_MODULE_FILE=lib/aarch64-linux-gnu/gtk-3.0/3.0.0/immodules/immodule
 export GDK_BACKEND=x11 
 export DISABLE_WAYLAND=1 
 export DCONF_PROFILE=/nonexistent
-export LIBGL_ALWAYS_SOFTWARE=1
 export XDG_CONFIG_HOME=/home/phablet/.config/signalut.pparent/
 
 if [ "$DISPLAY" = "" ]; then
@@ -22,10 +21,10 @@ fi
 export PATH=$PWD/bin:$PATH
 
 dpioptions="--high-dpi-support=1 --force-device-scale-factor=2.75"
-gpuoptions="--disable-gpu --disable-software-rasterizer  --in-process-gpu"
 sandboxoptions="--no-sandbox"
+gpuoptions="--use-gl=egl --enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-features=UseSkiaRenderer,VaapiVideoDecoder --disable-frame-rate-limit --disable-gpu-vsync --enable-oop-rasterization"
 
 #Open a dummy qt gui app to realease lomiri from its waiting
 (sleep.sh; $PWD/bin/xdg-open)&
 
-exec ./opt/Signal/Signal $dpioptions $gpuoptions $sandboxoptions
+exec ./opt/Signal/Signal $dpioptions $sandboxoptions $gpuoptions
